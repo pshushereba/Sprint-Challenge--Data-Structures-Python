701412b703e86c73c1bdc4e210536762eb8b5c53
Patrick Shushereba <pshushereba@gmail.com>
2020-05-30 10:09:11 -0700

    Finished ring buffer and MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,13 +2,17 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = []
+        self.oldest = 0
 
     def append(self, item):
-        if len(self.storage) == self.capacity:
-          self.storage.pop(0)
+        if len(self.storage) < self.capacity:
           self.storage.append(item)
         else:
-          self.storage.append(item)
+          self.storage[self.oldest] = item
+          if self.oldest < len(self.storage) - 1:
+            self.oldest += 1
+          else:
+            self.oldest = 0
 
     def get(self):
         return self.storage
